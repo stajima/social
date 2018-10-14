acc479213a5c82c0a4de82a7b64604d42b7413da
Shane Tajima <shanetajima@gmail.com>
2018-10-13 17:12:09 -0700

    Setup webpack dev server and hot modules

--- configs/webpack.dev.js
+++ configs/webpack.dev.js
@@ -1,7 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
   watch: true,
+  devServer: {
+    hot: true,
+    contentBase: path.join(__dirname, '../client', 'dist'),
+    port: 8080,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: path.join(__dirname, '../client', 'dist', 'index.html'),
+      template: path.join(__dirname, '../client', 'dist', 'index.html'),
+      inject: true,
+    }),
+  ],
 });
