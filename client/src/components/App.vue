416d6c082e8e113e5a14220bac0cf1e1b7b8503b
Shane Tajima <shanetajima@gmail.com>
2018-10-13 22:44:08 -0700

    Add registration view

--- client/src/components/App.vue
+++ client/src/components/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <h1>App.js</h1>
-    <app-view />
+    <registration v-if="!loggedIn" />
+    <app-view v-else />
   </div>
 </template>
 
 <script>
 import AppView from './AppView.vue'
+import Registration from './Registration.vue'
+
 export default {
   components: {
-    AppView
+    AppView,
+    Registration,
+  },
+  data: function () {
+    return {
+      loggedIn: false,
+    }
   }
 }
 </script>
