50a8d08ca6c3b08e7e1628d009c5d66fd7148c70
Shane Tajima <shanetajima@gmail.com>
2018-10-13 22:10:39 -0700

    Get vue template setup

---
+++ client/src/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>App.js</h1>
+    <app-view />
+  </div>
+</template>
+
+<script>
+import AppView from './AppView.vue'
+export default {
+  components: {
+    AppView
+  }
+}
+</script>
---
+++ client/src/components/AppView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello Shane!</h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
