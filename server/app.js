b0e7ad95787782055e36c53ec4bc34bb810c2f66
Shane Tajima <shanetajima@gmail.com>
2018-10-15 11:35:28 -0700

    Add get endpoint

--- server/app.js
+++ server/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
+const { getUserData } = require('./database');
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('/api/users/:id', (req, res, next) => {
+  console.log('get::');
+  getUserData(req.params.id, (error, data) => {
+    if (error) {
+      next(error);
+    }
+    res.send(data).end();
+  });
+});
+
+app.delete('/api/users/:id', (req, res) => {
+  console.log('delete::');
+
+});
 
 module.exports = app;
