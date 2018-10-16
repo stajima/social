742d383f84b9565a8e93959ad2924d8f53c38f94
Shane Tajima <shanetajima@gmail.com>
2018-10-15 20:18:18 -0700

    Remove about for MVP

--- client/src/components/About.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <h1>Vue Todo List</h1>
-    <small>By Shane Tajima</small>
-    <p>This is a Todo List using Vue.js, NodeJS, ExpressJS, eslint, and Webpack.</p>
-  </div>
-</template>
